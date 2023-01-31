f7332bcb4fe75e401deba01fee81ca75a02654ac
RNA <rnaryhn@gmail.com>
2023-01-31 17:01:18 +0300

    service created

--- src/service/Service.java
+++ src/service/Service.java
@@ -5,12 +5,7 @@ import entity.House;
 import entity.SummerHouse;
 import entity.Villa;
 import generator.Generator;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-public  class Service {
+public  final class Service {
 
     Generator generator = new Generator();
     public void sumPrices(BaseHouse[] baseHouses) {
@@ -30,29 +25,31 @@ public  class Service {
     }
 
     public void whichTypeHouseIsItForRoomAndSaloon(int room, int saloon) {
-        List<BaseHouse> baseHouseList = new ArrayList<>();
-        List<House> houseList = new ArrayList<>();
-        List<Villa> villaList = new ArrayList<>();
-        List<SummerHouse> summerHouseList = new ArrayList<>();
+
+        House[] houseList = generator.createAllHouses();
+        Villa[] villaList = generator.createAllVillas();
+        SummerHouse[] summerHouseList = generator.createAllSummerHouses();
+
+
         for (House house : houseList) {
-            if(house.getSaloon() <= 1 && house.getRoom() <= 4){
-                baseHouseList.add(house);
+            if(house.getSaloon() == saloon && house.getRoom() == room){
+
+                System.out.println("This is a house. ");
             }
-            System.out.println("This is a house. ");
+
         }
 
         for (Villa villa : villaList) {
-            if(villa.getSaloon() <= 3 && villa.getRoom() <= 10){
-                baseHouseList.add(villa);
+            if(villa.getSaloon() == saloon && villa.getRoom() == room){
+                System.out.println("This is a villa.");
             }
-            System.out.println("This is a villa.");
-        }
 
+        }
         for (SummerHouse summerHouse : summerHouseList) {
-            if(summerHouse.getSaloon() <= 2 && summerHouse.getRoom() <= 3){
-                baseHouseList.add(summerHouse);
+            if(summerHouse.getSaloon() == saloon && summerHouse.getRoom() == room){
+                System.out.println("This is a summer house.");
             }
-            System.out.println("This is a summer house.");
+
         }
     }
 
